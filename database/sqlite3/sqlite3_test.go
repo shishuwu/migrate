51e87e6169fd28d2a7c11608fcf568bc2b9a4bde
shuwu-shi <shuwu.shi@hp.com>
2020-07-28 15:17:50 +0800

    replace go-sqlite3 with sqlcipher

--- database/sqlite3/sqlite3_test.go
+++ database/sqlite3/sqlite3_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	dt "github.com/golang-migrate/migrate/v4/database/testing"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/mutecomm/go-sqlcipher/v4"
 )
 
 func Test(t *testing.T) {
